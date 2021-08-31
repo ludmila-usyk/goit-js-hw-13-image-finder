f7ce72cb4d2d5c4af1f093a4502f2317d335d890
ludmila <milausik@ukr.net>
2021-09-01 00:18:30 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,2 +1,114 @@
 import './sass/main.scss';
-import './js/fetchCountries';
+import refs from './refs.js';
+import photoCardsTpl from './template.hbs';
+import { error, defaultModules } from '../node_modules/@pnotify/core/dist/PNotify.js';
+import * as PNotifyMobile from '../node_modules/@pnotify/mobile/dist/PNotifyMobile.js';
+import '@pnotify/core/dist/BrightTheme.css';
+
+//переменные
+const BASE_URL = 'https://pixabay.com/api/';
+const KEY = '23121474-dc8d36b74d53a13d4dcab8680';
+let page = 1;
+let searchValue = '';//поиск
+
+
+// //посылаем запрос на бекенд
+// const handlerSubmit = (e) => {
+//   e.preventDefault()
+  
+//   //задаем поиск
+//   if (searchValue === refs.input.value) return;
+//   searchValue = refs.input.value;
+
+//   fetch(`${BASE_URL}?image_type=photo&orientation=horizontal&q=${searchValue}&page=${page}&per_page=12&key=${KEY}`)
+//   .then(response => response.json())
+//   .then(photo => renderPhoto(photo.hits))
+//   .catch(err => console.log(err))
+// }
+
+//посылаем запрос на бекенд
+const handlerSubmit = (e) => {
+    e.preventDefault()
+    
+    //задаем поиск
+    if (searchValue === refs.input.value) return;
+    searchValue = refs.input.value;
+  
+    fetch(`${BASE_URL}?image_type=photo&orientation=horizontal&q=${searchValue}&page=${page}&per_page=12&key=${KEY}`)
+    .then(response => response.json())
+    .then(photo => {
+       if (photo.hits.length === 0) {
+           return error({
+            text: 'Check your request.',
+            delay: 1000,
+          });
+       } else {
+           clearGallery();
+            renderPhoto(photo.hits);
+       }
+    })
+    .then(() => page++)
+    .then(clearContent)
+    .catch(err => {
+        defaultModules.set(PNotifyMobile, {});
+        clearGallery();
+        // error({
+        //     text: 'Nothing found',
+        //     delay: 1000,
+        // });
+    })
+  };
+
+refs.form.addEventListener('submit', handlerSubmit);
+refs.loadMore.addEventListener('click', loadMore);
+
+// function createGallery (arr) {
+//   for(let obj of arr) {
+//     createImg(obj)
+//   }
+// }
+
+// function createImg(obj) {
+//   const img = document.createElement('img')
+//   img.src = obj.webformatURL;
+//   refs.gallery.appendChild(img)
+// };
+
+function renderPhoto(photo) {
+    refs.gallery.insertAdjacentHTML('beforeend', photoCardsTpl(photo));
+  }
+
+function clearGallery() {
+    refs.gallery.innerHTML = '';
+};
+
+function clearContent() {
+    refs.input.value ='';
+};
+
+function loadMore (e) { //добавляет елементы
+    e.preventDefault();
+    
+    fetch(`${BASE_URL}?image_type=photo&orientation=horizontal&q=${searchValue}&page=${page}&per_page=12&key=${KEY}`)
+    .then(response => response.json())
+    .then(photo => {
+       if (photo.hits.length === 0) {
+           return error({
+            text: 'Check your request.',
+            delay: 1000,
+          });
+       } else {
+            renderPhoto(photo.hits);
+       }
+    })
+    .then(() => page++)
+    // .then(clearContent)
+    .catch(err => {
+        defaultModules.set(PNotifyMobile, {});
+        clearGallery();
+        error({
+            text: 'Nothing found',
+            delay: 1000,
+        });
+    })
+};
---
+++ src/refs.js
@@ -0,0 +1,7 @@
+export default {
+    input: document.querySelector('.input'),
+    form: document.querySelector('.search-form'),
+    search: document.querySelector('.search'),
+    gallery: document.querySelector('.gallery'),
+    loadMore: document.querySelector('#load-more')
+}
